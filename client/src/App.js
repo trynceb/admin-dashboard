f2d389afae7f11e8d32750d58158b322ce1ebae6
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-16 15:15:53 -0400

    create layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,10 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { themeSettings } from "theme";
+import Dashboard from "scenes/dashboard"
+import Layout from "scenes/layout"
 
 function App() {
   const mode = useSelector((state) => state.global.mode)
@@ -10,9 +13,17 @@ function App() {
 
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
