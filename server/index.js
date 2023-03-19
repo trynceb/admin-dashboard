0728f05b343e2d2f4219c445c48769ae505f77e2
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-19 16:31:04 -0400

    add Product and Product Stat models

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ mongoose.connect(process.env.MONGO_URL, {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
 
     /* ONLY ADD DATA ONE TIME */
-    Product.insertMany(dataProduct)
-    ProductStat.insertMany(dataProductStat)
+    // Product.insertMany(dataProduct)
+    // ProductStat.insertMany(dataProductStat)
     // User.insertMany(dataUser)
 }).catch((error) => console.log(`${error} did not connect`))
