81162b4f66a3ca9139a35fc3c1df0d21e2e7260c
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-21 08:43:55 -0400

    create transactions page

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -7,7 +7,6 @@ import { DataGrid } from "@mui/x-data-grid"
 const Customers = () => {
     const theme = useTheme()
     const { data, isLoading } = useGetCustomersQuery()
-    console.log("🚀 data:", data)
 
     const columns = [
         {
